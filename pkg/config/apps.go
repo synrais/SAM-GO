c24e2334fd56ba785b82963d73c296541aa4dc89
synrais <78070154+synrais@users.noreply.github.com>
2025-09-07 08:45:38 +1000

    Update apps.go

--- pkg/config/apps.go
+++ pkg/config/apps.go
@@ -1,24 +1,24 @@
 package config
 
-const UserConfigEnv = "SAM_CONFIG"
+const UserConfigEnv  = "SAM_CONFIG"
 const UserAppPathEnv = "SAM_APP_PATH"
 
-const ActiveGameFile = TempFolder + "/ACTIVEGAME"
-const SearchDbFile = SdFolder + "/search.db"
-const PlayLogDbFile = SdFolder + "/playlog.db"
+const ActiveGameFile  = TempFolder + "/ACTIVEGAME"
+const SearchDbFile    = SdFolder + "/search.db"
+const PlayLogDbFile   = SdFolder + "/playlog.db"
 
 const PidFileTemplate = TempFolder + "/%s.pid"
 const LogFileTemplate = TempFolder + "/%s.log"
 
 const ScriptsConfigFolder = ScriptsFolder + "/.config"
-const MrextConfigFolder = ScriptsConfigFolder + "/mrext"
+const SAMConfigFolder     = ScriptsConfigFolder + "/sam"
 
-const ArcadeDBUrl = "https://api.github.com/repositories/521644036/contents/ArcadeDatabase_CSV"
-const ArcadeDBFile = MrextConfigFolder + "/ArcadeDatabase.csv"
+const ArcadeDBUrl  = "https://api.github.com/repositories/521644036/contents/ArcadeDatabase_CSV"
+const ArcadeDBFile = SAMConfigFolder + "/ArcadeDatabase.csv"
 
 const NfcDatabaseFile = SdFolder + "/nfc.csv"
 const NfcLastScanFile = TempFolder + "/NFCSCAN"
 
-const GamesDb = ScriptsConfigFolder + "/sam/games.db"
+const GamesDb = SAMConfigFolder + "/games.db"
 
 const LastLaunchFile = SdFolder + "/.LASTLAUNCH.mgl"
