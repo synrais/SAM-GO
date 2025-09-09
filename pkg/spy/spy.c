be51dc80f6b77008295f62856bfee71ec0e187df
synrais <78070154+synrais@users.noreply.github.com>
2025-09-09 21:42:55 +1000

    Update spy.c

--- pkg/spy/spy.c
+++ pkg/spy/spy.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <linux/joystick.h>
-#include <linux/input.h>
-#include <sys/ioctl.h>
-#include <poll.h>
+#include "axbtnmap.h"
 
 #define MAX_DEVICES 16
 
-struct js_device {
+typedef struct {
     int fd;
     char name[128];
-    int axes;
-    int buttons;
-    __u8 axmap[ABS_MAX + 1];
-    __u16 btnmap[KEY_MAX - BTN_MISC + 1];
-};
+    unsigned char axes;
+    unsigned char buttons;
+    uint8_t axmap[AXMAP_SIZE];
+    uint16_t btnmap[BTNMAP_SIZE];
+    int axis_state[AXMAP_SIZE];
+    int button_state[BTNMAP_SIZE];
+} js_device;
 
-static struct js_device devices[MAX_DEVICES];
-static int device_count = 0;
+static js_device devices[MAX_DEVICES];
+static int dev_count = 0;
 
 void spy_scan_devices() {
     char path[64];
@@ -30,46 +30,52 @@ void spy_scan_devices() {
         int fd = open(path, O_RDONLY | O_NONBLOCK);
         if (fd < 0) continue;
 
-        struct js_device dev;
-        memset(&dev, 0, sizeof(dev));
-        dev.fd = fd;
-        ioctl(fd, JSIOCGNAME(sizeof(dev.name)), dev.name);
-        ioctl(fd, JSIOCGAXES, &dev.axes);
-        ioctl(fd, JSIOCGBUTTONS, &dev.buttons);
-        ioctl(fd, JSIOCGAXMAP, dev.axmap);
-        ioctl(fd, JSIOCGBTNMAP, dev.btnmap);
+        js_device *dev = &devices[dev_count++];
+        dev->fd = fd;
+        ioctl(fd, JSIOCGNAME(sizeof(dev->name)), dev->name);
+        ioctl(fd, JSIOCGAXES, &dev->axes);
+        ioctl(fd, JSIOCGBUTTONS, &dev->buttons);
+        getaxmap(fd, dev->axmap);
+        getbtnmap(fd, dev->btnmap);
 
-        devices[device_count++] = dev;
-
-        printf("Monitoring %s (%s)\n", path, dev.name);
-        printf("  Axes: %d  Buttons: %d\n", dev.axes, dev.buttons);
+        printf("Monitoring %s (%s)\n", path, dev->name);
+        printf("  Axes: %d  Buttons: %d\n", dev->axes, dev->buttons);
     }
 }
 
 void spy_loop() {
-    struct pollfd fds[MAX_DEVICES];
-    for (int i = 0; i < device_count; i++) {
-        fds[i].fd = devices[i].fd;
-        fds[i].events = POLLIN;
-    }
-
+    struct js_event e;
     while (1) {
-        int ret = poll(fds, device_count, 1000);
-        if (ret <= 0) continue;
-
-        for (int i = 0; i < device_count; i++) {
-            if (!(fds[i].revents & POLLIN)) continue;
-
-            struct js_event e;
-            while (read(fds[i].fd, &e, sizeof(e)) > 0) {
+        for (int d = 0; d < dev_count; d++) {
+            js_device *dev = &devices[d];
+            while (read(dev->fd, &e, sizeof(e)) > 0) {
                 e.type &= ~JS_EVENT_INIT;
                 if (e.type == JS_EVENT_AXIS) {
-                    printf("[%s] Axis %d = %d\n", devices[i].name, e.number, e.value);
+                    int axis = dev->axmap[e.number];
+                    dev->axis_state[axis] = e.value;
                 } else if (e.type == JS_EVENT_BUTTON) {
-                    printf("[%s] Button %d = %d\n", devices[i].name, e.number, e.value);
+                    int btn = dev->btnmap[e.number];
+                    dev->button_state[btn] = e.value;
                 }
-                fflush(stdout);
             }
+
+            // Full RetroSpy-style dump
+            printf("[%s] Axes[", dev->name);
+            for (int i = 0; i < dev->axes; i++) {
+                int axis = dev->axmap[i];
+                printf("%s=%d ",
+                    axis < AXMAP_SIZE ? axnames[axis] : "AXIS?",
+                    dev->axis_state[axis]);
+            }
+            printf("] Buttons[");
+            for (int i = 0; i < dev->buttons; i++) {
+                int btn = dev->btnmap[i];
+                printf("%s=%d ",
+                    btn < BTNMAP_SIZE ? btnnames[btn] : "BTN?",
+                    dev->button_state[btn]);
+            }
+            printf("]\n");
         }
+        usleep(16000); // ~60 Hz
     }
 }
