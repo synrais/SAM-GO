a9da10ebdf0c04d4af18c26260930a1f2ea19c9d
synrais <78070154+synrais@users.noreply.github.com>
2025-09-09 20:46:07 +1000

    Update spy.go

--- pkg/spy/spy.go
+++ pkg/spy/spy.go
@@ -4,38 +4,49 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
-	"sync"
 	"syscall"
+	"unsafe"
 
-	"github.com/fsnotify/fsnotify"
 	"golang.org/x/sys/unix"
 )
 
 const (
-	ReportSize  = 64
-	Deadzone    = 15
+	// HID
+	ReportSize = 64
+	Deadzone   = 15
+
+	// Joystick constants
 	JS_EVENT_BUTTON = 0x01
 	JS_EVENT_AXIS   = 0x02
 	JS_EVENT_INIT   = 0x80
+
+	JSIOCGVERSION = 0x80046a01
+	JSIOCGAXES    = 0x80016a11
+	JSIOCGBUTTONS = 0x80016a12
+	JSIOCGNAME    = 0x81006a13
+	JSIOCGAXMAP   = 0x80406a32
+	JSIOCGBTNMAP  = 0x80406a34
 )
 
-type Device struct {
-	fd     int
-	path   string
-	kind   string // "js" or "hid"
-	name   string
-	axes   map[string]int16
-	buttons map[string]bool
-	axmap  []uint32
-	btnmap []uint32
+// Lookup tables (RetroSpy style)
+var axisNames = map[uint32]string{
+	0x00: "ABS_X", 0x01: "ABS_Y", 0x02: "ABS_Z",
+	0x03: "ABS_RX", 0x04: "ABS_RY", 0x05: "ABS_RZ",
+	0x10: "ABS_HAT0X", 0x11: "ABS_HAT0Y",
+	0x12: "ABS_HAT1X", 0x13: "ABS_HAT1Y",
 }
 
-// ---------------- Keyboard/mouse maps (from Python) ----------------
-var Keymap = map[byte]string{
+var buttonNames = map[uint32]string{
+	0x120: "BTN_SOUTH", 0x121: "BTN_EAST", 0x122: "BTN_C", 0x123: "BTN_NORTH",
+	0x124: "BTN_WEST", 0x125: "BTN_Z", 0x126: "BTN_TL", 0x127: "BTN_TR",
+	0x128: "BTN_TL2", 0x129: "BTN_TR2", 0x12a: "BTN_SELECT", 0x12b: "BTN_START",
+	0x12c: "BTN_MODE", 0x12d: "BTN_THUMBL", 0x12e: "BTN_THUMBR",
+}
+
+var keymap = map[byte]string{
 	0x04: "A", 0x05: "B", 0x06: "C", 0x07: "D", 0x08: "E", 0x09: "F",
 	0x0A: "G", 0x0B: "H", 0x0C: "I", 0x0D: "J", 0x0E: "K", 0x0F: "L",
 	0x10: "M", 0x11: "N", 0x12: "O", 0x13: "P", 0x14: "Q", 0x15: "R",
@@ -46,197 +57,190 @@ var Keymap = map[byte]string{
 	0x4F: "Right", 0x50: "Left", 0x51: "Down", 0x52: "Up",
 }
 
-// ---------------- Joystick setup (RetroSpy style) ----------------
-func setupJsDevice(path string) (*Device, error) {
-	fd, err := unix.Open(path, unix.O_RDONLY|unix.O_NONBLOCK, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	// Get name
-	buf := make([]byte, 128)
-	if _, _, errno := unix.Syscall(unix.SYS_IOCTL, uintptr(fd), uintptr(unix.JSIOCGNAME(len(buf))), uintptr((unsafe.Pointer(&buf[0])))); errno != 0 {
-		return nil, errno
-	}
-	name := string(buf[:bytes.IndexByte(buf, 0)])
-
-	fmt.Printf("Monitoring %s (joystick: %s)\n", path, name)
-	return &Device{
-		fd: fd, path: path, kind: "js", name: name,
-		axes: make(map[string]int16), buttons: make(map[string]bool),
-	}, nil
+// Device structs
+type Device struct {
+	kind    string
+	fd      int
+	path    string
+	name    string
+	axes    map[string]int16
+	buttons map[string]bool
+	axmap   []uint32
+	btnmap  []uint32
 }
 
-// ---------------- HID decoding ----------------
-func decodeHidraw(path string, data []byte) string {
-	if len(data) == 8 { // Keyboard report
+func decodeKeyboard(data []byte) string {
+	if data[0] == 0x20 && len(data) >= 5 {
+		k := data[len(data)-1]
+		return fmt.Sprintf("KEYBOARD pressed: %s", keymap[k])
+	}
+	if len(data) == 8 {
 		var pressed []string
 		for _, code := range data[2:] {
 			if code != 0 {
-				if key, ok := Keymap[code]; ok {
-					pressed = append(pressed, key)
-				} else {
-					pressed = append(pressed, fmt.Sprintf("0x%02x", code))
-				}
+				pressed = append(pressed, keymap[code])
 			}
 		}
 		if len(pressed) > 0 {
-			return fmt.Sprintf("KEYBOARD pressed: %s", strings.Join(pressed, ", "))
+			return "KEYBOARD pressed: " + strings.Join(pressed, ", ")
 		}
 		return "KEYBOARD released"
 	}
-	if len(data) >= 4 { // Mouse
-		buttons := data[1]
-		x := int16(binary.LittleEndian.Uint16(data[2:4]))
-		y := int16(binary.LittleEndian.Uint16(data[4:6]))
-		if buttons == 0 && x == 0 && y == 0 {
-			return ""
-		}
-		return fmt.Sprintf("MOUSE buttons=0x%x dx=%d dy=%d", buttons, x, y)
-	}
-	return fmt.Sprintf("HIDRAW raw: %v", data)
+	return ""
 }
 
-// ---------------- Event loop ----------------
-func handleJsEvent(dev *Device) {
-	buf := make([]byte, 8)
-	for {
-		n, err := unix.Read(dev.fd, buf)
-		if err != nil {
-			if err == unix.EAGAIN {
-				return
-			}
-			fmt.Printf("%s closed\n", dev.path)
-			unix.Close(dev.fd)
-			return
-		}
-		if n != 8 {
-			return
-		}
-		var t uint32
-		var val int16
-		var etype, num byte
-		binary.Read(bytes.NewReader(buf), binary.LittleEndian, &t)
-		binary.Read(bytes.NewReader(buf[4:6]), binary.LittleEndian, &val)
-		etype = buf[6]
-		num = buf[7]
-		etype &= ^JS_EVENT_INIT
-
-		if etype == JS_EVENT_AXIS {
-			dev.axes[fmt.Sprintf("AXIS_%d", num)] = val
-		} else if etype == JS_EVENT_BUTTON {
-			dev.buttons[fmt.Sprintf("BTN_%d", num)] = val != 0
-		}
-
-		var axs []string
-		for k, v := range dev.axes {
-			axs = append(axs, fmt.Sprintf("%s=%d", k, v))
-		}
-		var btns []string
-		for k, v := range dev.buttons {
-			btns = append(btns, fmt.Sprintf("%s=%t", k, v))
-		}
-		fmt.Printf("[%s] Axes[%s] Buttons[%s]\n", dev.name, strings.Join(axs, ", "), strings.Join(btns, ", "))
+func decodeMouse(data []byte) string {
+	if len(data) < 3 {
+		return ""
 	}
+	buttons := data[1]
+	x := int16(binary.LittleEndian.Uint16(data[2:4]))
+	y := int16(binary.LittleEndian.Uint16(data[4:6]))
+	if buttons == 0 && x == 0 && y == 0 {
+		return ""
+	}
+	btns := []string{}
+	if buttons&1 != 0 {
+		btns = append(btns, "L")
+	}
+	if buttons&2 != 0 {
+		btns = append(btns, "R")
+	}
+	if buttons&4 != 0 {
+		btns = append(btns, "M")
+	}
+	return fmt.Sprintf("MOUSE buttons=%v dx=%d dy=%d", btns, x, y)
 }
 
-func handleHidEvent(dev *Device) {
-	buf := make([]byte, ReportSize)
-	n, err := unix.Read(dev.fd, buf)
-	if err != nil || n == 0 {
-		return
+func setupJS(path string) *Device {
+	fd, err := unix.Open(path, os.O_RDONLY|unix.O_NONBLOCK, 0)
+	if err != nil {
+		fmt.Println("Cannot open js:", err)
+		return nil
 	}
-	msg := decodeHidraw(dev.path, buf[:n])
-	if msg != "" {
-		fmt.Printf("[%s] %s\n", dev.path, msg)
+	// Name
+	nameBuf := make([]byte, 128)
+	unix.IoctlGetInt(fd, JSIOCGNAME) // just trigger
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), uintptr(JSIOCGNAME), uintptr(unsafe.Pointer(&nameBuf[0])))
+	if errno != 0 {
+		return nil
 	}
+	name := string(bytes.TrimRight(nameBuf, "\x00"))
+
+	// Axes/buttons
+	axmap := make([]uint32, 64)
+	btnmap := make([]uint32, 256)
+	unix.Ioctl(fd, JSIOCGAXMAP, uintptr(unsafe.Pointer(&axmap[0])))
+	unix.Ioctl(fd, JSIOCGBTNMAP, uintptr(unsafe.Pointer(&btnmap[0])))
+
+	stateAxes := make(map[string]int16)
+	for _, a := range axmap {
+		stateAxes[axisNames[a]] = 0
+	}
+	stateButtons := make(map[string]bool)
+	for _, b := range btnmap {
+		stateButtons[buttonNames[b]] = false
+	}
+
+	fmt.Printf("Monitoring %s (joystick: %s)\n", path, name)
+	return &Device{kind: "js", fd: fd, path: path, name: name, axes: stateAxes, buttons: stateButtons, axmap: axmap, btnmap: btnmap}
 }
 
-// ---------------- Hotplug watcher ----------------
-func Run(args []string) {
+func setupHID(path string) *Device {
+	fd, err := unix.Open(path, os.O_RDONLY|unix.O_NONBLOCK, 0)
+	if err != nil {
+		return nil
+	}
+	fmt.Printf("Monitoring %s (hidraw)\n", path)
+	return &Device{kind: "hid", fd: fd, path: path}
+}
+
+func handleJSEvent(d *Device, evt []byte) {
+	var t uint32
+	var val int16
+	var etype, num uint8
+	binary.Read(bytes.NewReader(evt), binary.LittleEndian, &t)
+	binary.Read(bytes.NewReader(evt[4:]), binary.LittleEndian, &val)
+	etype = evt[6]
+	num = evt[7]
+	etype &= ^JS_EVENT_INIT
+
+	if etype == JS_EVENT_AXIS {
+		axis := axisNames[d.axmap[num]]
+		d.axes[axis] = val
+	} else if etype == JS_EVENT_BUTTON {
+		btn := buttonNames[d.btnmap[num]]
+		d.buttons[btn] = val != 0
+	}
+
+	axStr := []string{}
+	for k, v := range d.axes {
+		axStr = append(axStr, fmt.Sprintf("%s=%d", k, v))
+	}
+	btnStr := []string{}
+	for k, v := range d.buttons {
+		if v {
+			btnStr = append(btnStr, fmt.Sprintf("%s=1", k))
+		} else {
+			btnStr = append(btnStr, fmt.Sprintf("%s=0", k))
+		}
+	}
+	fmt.Printf("[%s] Axes[%s] Buttons[%s]\n", d.name, strings.Join(axStr, ", "), strings.Join(btnStr, ", "))
+}
+
+func Run() {
 	fmt.Println("Starting Spy monitor…")
-	var mu sync.Mutex
-	devices := make(map[int]*Device)
-
-	openDevice := func(path string) {
-		if strings.HasPrefix(filepath.Base(path), "js") {
-			if dev, err := setupJsDevice(path); err == nil {
-				mu.Lock()
-				devices[dev.fd] = dev
-				mu.Unlock()
-			}
-		} else if strings.HasPrefix(filepath.Base(path), "hidraw") {
-			fd, err := unix.Open(path, unix.O_RDONLY|unix.O_NONBLOCK, 0)
-			if err == nil {
-				fmt.Printf("Monitoring %s (hidraw)\n", path)
-				devices[fd] = &Device{fd: fd, path: path, kind: "hid"}
-			}
-		}
-	}
+	devices := []*Device{}
 
 	// Initial scan
-	files, _ := filepath.Glob("/dev/input/js*")
-	for _, f := range files {
-		openDevice(f)
-	}
-	files, _ = filepath.Glob("/dev/hidraw*")
-	for _, f := range files {
-		openDevice(f)
-	}
-
-	// Watch /dev and /dev/input for hotplug
-	watcher, _ := fsnotify.NewWatcher()
-	defer watcher.Close()
-	watcher.Add("/dev")
-	watcher.Add("/dev/input")
-
-	go func() {
-		for ev := range watcher.Events {
-			if ev.Op&(fsnotify.Create) != 0 {
-				openDevice(ev.Name)
-			}
-			if ev.Op&(fsnotify.Remove) != 0 {
-				// Device removed
-				mu.Lock()
-				for fd, d := range devices {
-					if d.path == ev.Name {
-						unix.Close(fd)
-						delete(devices, fd)
-						fmt.Printf("Closed %s\n", ev.Name)
-					}
-				}
-				mu.Unlock()
+	filepath.Walk("/dev/input", func(path string, info os.FileInfo, err error) error {
+		if strings.HasPrefix(filepath.Base(path), "js") {
+			if dev := setupJS(path); dev != nil {
+				devices = append(devices, dev)
 			}
 		}
-	}()
+		return nil
+	})
+	filepath.Walk("/dev", func(path string, info os.FileInfo, err error) error {
+		if strings.HasPrefix(filepath.Base(path), "hidraw") {
+			if dev := setupHID(path); dev != nil {
+				devices = append(devices, dev)
+			}
+		}
+		return nil
+	})
 
-	// Poll loop
+	fds := []unix.PollFd{}
+	for _, d := range devices {
+		fds = append(fds, unix.PollFd{Fd: int32(d.fd), Events: unix.POLLIN})
+	}
+
 	for {
-		mu.Lock()
-		var fds []unix.PollFd
-		for fd := range devices {
-			fds = append(fds, unix.PollFd{Fd: int32(fd), Events: unix.POLLIN})
-		}
-		mu.Unlock()
-		if len(fds) == 0 {
-			continue
-		}
 		_, err := unix.Poll(fds, -1)
 		if err != nil {
 			continue
 		}
-		mu.Lock()
-		for _, p := range fds {
-			if p.Revents&unix.POLLIN != 0 {
-				if dev, ok := devices[int(p.Fd)]; ok {
-					if dev.kind == "js" {
-						handleJsEvent(dev)
-					} else {
-						handleHidEvent(dev)
+		for i, fd := range fds {
+			if fd.Revents&unix.POLLIN != 0 {
+				d := devices[i]
+				if d.kind == "hid" {
+					buf := make([]byte, ReportSize)
+					n, _ := unix.Read(d.fd, buf)
+					if n > 0 {
+						if s := decodeKeyboard(buf[:n]); s != "" {
+							fmt.Printf("[%s] %s\n", d.path, s)
+						} else if s := decodeMouse(buf[:n]); s != "" {
+							fmt.Printf("[%s] %s\n", d.path, s)
+						}
+					}
+				} else {
+					evt := make([]byte, 8)
+					n, _ := unix.Read(d.fd, evt)
+					if n == 8 {
+						handleJSEvent(d, evt)
 					}
 				}
 			}
 		}
-		mu.Unlock()
 	}
 }
