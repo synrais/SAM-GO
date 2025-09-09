c96f514420f76233afc221934f7ff78432f049aa
synrais <78070154+synrais@users.noreply.github.com>
2025-09-09 20:54:02 +1000

    Update spy.go

--- pkg/spy/spy.go
+++ pkg/spy/spy.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,23 +12,20 @@ import (
 	"unsafe"
 )
 
+// Constants from linux/joystick.h
 const (
-	reportSize   = 64
-	deadzone     = 15
-	scanInterval = 2 * time.Second
-
-	// joystick event types
 	jsEventButton = 0x01
 	jsEventAxis   = 0x02
 	jsEventInit   = 0x80
 
-	// ioctl constants
-	JSIOCGVERSION = 0x80046a01
-	JSIOCGAXES    = 0x80016a11
-	JSIOCGBUTTONS = 0x80016a12
-	JSIOCGNAME    = 0x81006a13
-	JSIOCGAXMAP   = 0x80406a32
-	JSIOCGBTNMAP  = 0x80406a34
+	jsIOCGVersion  = 0x80046a01
+	jsIOCGAxes     = 0x80016a11
+	jsIOCButtons   = 0x80016a12
+	jsIOCGName     = 0x81006a13
+	jsIOCGAXMAP    = 0x80406a32
+	jsIOCGBTNMAP   = 0x80406a34
+	JSIOCGBTNMAP_L = 0x80406a34
+	JSIOCGBTNMAP_S = 0x80406a33
 )
 
 var axisNames = map[uint32]string{
@@ -39,198 +35,206 @@ var axisNames = map[uint32]string{
 	0x12: "ABS_HAT1X", 0x13: "ABS_HAT1Y",
 }
 var buttonNames = map[uint32]string{
-	0x120: "BTN_SOUTH", 0x121: "BTN_EAST", 0x123: "BTN_NORTH", 0x124: "BTN_WEST",
-	0x126: "BTN_TL", 0x127: "BTN_TR", 0x128: "BTN_TL2", 0x129: "BTN_TR2",
-	0x12a: "BTN_SELECT", 0x12b: "BTN_START", 0x12c: "BTN_MODE",
-	0x12d: "BTN_THUMBL", 0x12e: "BTN_THUMBR",
+	0x120: "BTN_SOUTH", 0x121: "BTN_EAST", 0x122: "BTN_C", 0x123: "BTN_NORTH",
+	0x124: "BTN_WEST", 0x125: "BTN_Z", 0x126: "BTN_TL", 0x127: "BTN_TR",
+	0x128: "BTN_TL2", 0x129: "BTN_TR2", 0x12a: "BTN_SELECT", 0x12b: "BTN_START",
+	0x12c: "BTN_MODE", 0x12d: "BTN_THUMBL", 0x12e: "BTN_THUMBR",
 }
 
-var keymap = map[byte]string{
-	0x04: "A", 0x05: "B", 0x06: "C", 0x07: "D",
-	0x08: "E", 0x09: "F", 0x0A: "G", 0x0B: "H",
-	0x0C: "I", 0x0D: "J", 0x0E: "K", 0x0F: "L",
-	0x10: "M", 0x11: "N", 0x12: "O", 0x13: "P",
-	0x14: "Q", 0x15: "R", 0x16: "S", 0x17: "T",
-	0x18: "U", 0x19: "V", 0x1A: "W", 0x1B: "X",
-	0x1C: "Y", 0x1D: "Z",
-	0x1E: "1", 0x1F: "2", 0x20: "3", 0x21: "4", 0x22: "5",
-	0x23: "6", 0x24: "7", 0x25: "8", 0x26: "9", 0x27: "0",
-	0x28: "Enter", 0x29: "Esc", 0x2A: "Backspace",
-	0x2B: "Tab", 0x2C: "Space",
-	0x4F: "Right", 0x50: "Left", 0x51: "Down", 0x52: "Up",
+type jsEvent struct {
+	Time   uint32
+	Value  int16
+	Type   uint8
+	Number uint8
 }
 
-type device struct {
-	fd      *os.File
-	path    string
-	kind    string
-	name    string
-	axmap   []uint32
-	btnmap  []uint32
-	axes    map[string]int16
-	buttons map[string]byte
+type jsDevice struct {
+	fd       *os.File
+	path     string
+	name     string
+	axes     map[string]int16
+	buttons  map[string]bool
+	axmap    []uint32
+	btnmap   []uint32
+	numAxes  int
+	numBtns  int
 }
 
-func ioctl(fd uintptr, req, arg uintptr) error {
-	_, _, e := syscall.Syscall(syscall.SYS_IOCTL, fd, req, arg)
-	if e != 0 {
-		return e
+func ioctl(fd uintptr, cmd uintptr, arg unsafe.Pointer) error {
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, cmd, uintptr(arg))
+	if errno != 0 {
+		return errno
 	}
 	return nil
 }
 
-func setupJoystick(path string) (*device, error) {
+func setupJS(path string) (*jsDevice, error) {
 	fd, err := os.OpenFile(path, os.O_RDONLY|syscall.O_NONBLOCK, 0)
 	if err != nil {
 		return nil, err
 	}
 
+	// Name
 	nameBuf := make([]byte, 128)
-	if err := ioctl(fd.Fd(), JSIOCGNAME, uintptr(unsafe.Pointer(&nameBuf[0]))); err != nil {
-		fd.Close()
+	if err := ioctl(fd.Fd(), jsIOCGName, unsafe.Pointer(&nameBuf[0])); err != nil {
 		return nil, err
 	}
 	name := strings.TrimRight(string(nameBuf), "\x00")
 
-	axBuf := make([]byte, 64)
-	if err := ioctl(fd.Fd(), JSIOCGAXMAP, uintptr(unsafe.Pointer(&axBuf[0]))); err != nil {
-		fd.Close()
+	// Num axes
+	var numAxes uint8
+	if err := ioctl(fd.Fd(), jsIOCGAxes, unsafe.Pointer(&numAxes)); err != nil {
 		return nil, err
 	}
-	axmap := make([]uint32, len(axBuf)/4)
-	_ = binary.Read(bytes.NewReader(axBuf), binary.LittleEndian, &axmap)
 
-	btnBuf := make([]byte, 512)
-	if err := ioctl(fd.Fd(), JSIOCGBTNMAP, uintptr(unsafe.Pointer(&btnBuf[0]))); err != nil {
-		fd.Close()
+	// Num buttons
+	var numBtns uint8
+	if err := ioctl(fd.Fd(), jsIOCButtons, unsafe.Pointer(&numBtns)); err != nil {
 		return nil, err
 	}
-	btnmap := make([]uint32, len(btnBuf)/4)
-	_ = binary.Read(bytes.NewReader(btnBuf), binary.LittleEndian, &btnmap)
+
+	// Axis map
+	axmap := make([]uint32, numAxes)
+	if err := ioctl(fd.Fd(), jsIOCGAXMAP, unsafe.Pointer(&axmap[0])); err != nil {
+		return nil, err
+	}
+
+	// Button map (try multiple ioctl variants)
+	btnmap := make([]uint32, numBtns)
+	if err := ioctl(fd.Fd(), jsIOCGBTNMAP, unsafe.Pointer(&btnmap[0])); err != nil {
+		// try fallback
+		ioctl(fd.Fd(), JSIOCGBTNMAP_L, unsafe.Pointer(&btnmap[0]))
+		ioctl(fd.Fd(), JSIOCGBTNMAP_S, unsafe.Pointer(&btnmap[0]))
+	}
 
 	fmt.Printf("Monitoring %s (joystick: %s)\n", path, name)
+	fmt.Printf("  Axes: %d  Buttons: %d\n", numAxes, numBtns)
 
-	return &device{
-		fd:      fd,
-		path:    path,
-		kind:    "js",
-		name:    name,
-		axmap:   axmap,
-		btnmap:  btnmap,
-		axes:    make(map[string]int16),
-		buttons: make(map[string]byte),
-	}, nil
+	axes := make(map[string]int16)
+	for _, a := range axmap {
+		axes[axisNames[a]] = 0
+	}
+	buttons := make(map[string]bool)
+	for _, b := range btnmap {
+		buttons[buttonNames[b]] = false
+	}
+
+	return &jsDevice{fd, path, name, axes, buttons, axmap, btnmap, int(numAxes), int(numBtns)}, nil
 }
 
-func handleJSEvent(d *device, evt []byte) {
-	var t uint32
-	var val int16
-	var etype, num uint8
-	_ = binary.Read(bytes.NewReader(evt), binary.LittleEndian, &t)
-	_ = binary.Read(bytes.NewReader(evt[4:]), binary.LittleEndian, &val)
-	etype = evt[6]
-	num = evt[7]
-
-	// fix overflow by casting
-	if etype&jsEventInit != 0 {
-		etype &= ^uint8(jsEventInit)
-	}
-
-	if etype == jsEventAxis {
-		ax := axisNames[d.axmap[num]]
-		if ax == "" {
-			ax = fmt.Sprintf("AXIS_%d", num)
-		}
-		d.axes[ax] = val
-	} else if etype == jsEventButton {
-		btn := buttonNames[d.btnmap[num]]
-		if btn == "" {
-			btn = fmt.Sprintf("BTN_%d", num)
-		}
-		d.buttons[btn] = byte(val)
-	}
-	axs := []string{}
-	for k, v := range d.axes {
-		axs = append(axs, fmt.Sprintf("%s=%d", k, v))
-	}
-	btns := []string{}
-	for k, v := range d.buttons {
-		btns = append(btns, fmt.Sprintf("%s=%d", k, v))
-	}
-	fmt.Printf("[%s] Axes[%s] Buttons[%s]\n", d.name, strings.Join(axs, ", "), strings.Join(btns, ", "))
-}
-
-func handleHIDRaw(d *device) {
-	buf := make([]byte, reportSize)
-	n, err := d.fd.Read(buf)
-	if err != nil || n == 0 {
-		return
-	}
-	data := buf[:n]
-	if len(data) == 8 {
-		pressed := []string{}
-		for _, c := range data[2:] {
-			if c != 0 {
-				pressed = append(pressed, keymap[c])
-			}
-		}
-		if len(pressed) > 0 {
-			fmt.Printf("[%s] KEYBOARD pressed: %s\n", d.path, strings.Join(pressed, ", "))
-		}
-	}
-}
-
-func monitor(devs map[string]*device) {
+func (dev *jsDevice) readEvents() {
+	buf := make([]byte, int(unsafe.Sizeof(jsEvent{})))
 	for {
-		for path, d := range devs {
-			if d.kind == "js" {
-				buf := make([]byte, 8)
-				for {
-					_, err := d.fd.Read(buf)
-					if err == io.EOF || (err != nil && err != syscall.EAGAIN) {
-						break
-					}
-					if err != nil {
-						break
-					}
-					handleJSEvent(d, buf)
-				}
-			} else {
-				handleHIDRaw(d)
-			}
-			if _, err := os.Stat(path); os.IsNotExist(err) {
-				d.fd.Close()
-				delete(devs, path)
-				fmt.Println("Removed:", path)
-			}
+		n, err := dev.fd.Read(buf)
+		if n <= 0 || err != nil {
+			break
 		}
-		// hotplug: rescan
-		filepath.Walk("/dev", func(path string, info os.FileInfo, err error) error {
-			if strings.HasPrefix(path, "/dev/input/js") || strings.HasPrefix(path, "/dev/hidraw") {
-				if _, ok := devs[path]; !ok {
-					if strings.Contains(path, "js") {
-						jsdev, err := setupJoystick(path)
-						if err == nil {
-							devs[path] = jsdev
-						}
-					} else {
-						fd, err := os.OpenFile(path, os.O_RDONLY|syscall.O_NONBLOCK, 0)
-						if err == nil {
-							fmt.Println("Monitoring", path, "(hidraw)")
-							devs[path] = &device{fd: fd, path: path, kind: "hid"}
-						}
-					}
-				}
+		var e jsEvent
+		binary.Read(bytes.NewReader(buf), binary.LittleEndian, &e)
+		etype := e.Type & ^uint8(jsEventInit)
+
+		if etype == jsEventAxis && int(e.Number) < len(dev.axmap) {
+			name := axisNames[dev.axmap[e.Number]]
+			if name == "" {
+				name = fmt.Sprintf("AXIS_%d", e.Number)
 			}
-			return nil
-		})
-		time.Sleep(scanInterval)
+			dev.axes[name] = e.Value
+		} else if etype == jsEventButton && int(e.Number) < len(dev.btnmap) {
+			name := buttonNames[dev.btnmap[e.Number]]
+			if name == "" {
+				name = fmt.Sprintf("BTN_%d", e.Number)
+			}
+			dev.buttons[name] = e.Value != 0
+		}
+
+		axOut := []string{}
+		for k, v := range dev.axes {
+			axOut = append(axOut, fmt.Sprintf("%s=%d", k, v))
+		}
+		btnOut := []string{}
+		for k, v := range dev.buttons {
+			val := 0
+			if v {
+				val = 1
+			}
+			btnOut = append(btnOut, fmt.Sprintf("%s=%d", k, val))
+		}
+		fmt.Printf("[%s] Axes[%s] Buttons[%s]\n", dev.name, strings.Join(axOut, ", "), strings.Join(btnOut, ", "))
+	}
+}
+
+// HID device (just raw dump for now)
+type hidDevice struct {
+	fd   *os.File
+	path string
+}
+
+func setupHID(path string) (*hidDevice, error) {
+	fd, err := os.OpenFile(path, os.O_RDONLY|syscall.O_NONBLOCK, 0)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("Monitoring %s (hidraw)\n", path)
+	return &hidDevice{fd, path}, nil
+}
+
+func (dev *hidDevice) readEvents() {
+	buf := make([]byte, 64)
+	for {
+		n, err := dev.fd.Read(buf)
+		if n <= 0 || err != nil {
+			break
+		}
+		fmt.Printf("[%s] RAW: % x\n", dev.path, buf[:n])
 	}
 }
 
 func Run(args []string) {
 	fmt.Println("Starting Spy monitor…")
-	devs := make(map[string]*device)
-	monitor(devs)
-}
 
+	jsdevs := []*jsDevice{}
+	hiddevs := []*hidDevice{}
+
+	// Initial scan
+	scan := func() {
+		filepath.Walk("/dev/input", func(path string, info os.FileInfo, err error) error {
+			if strings.HasPrefix(filepath.Base(path), "js") {
+				for _, d := range jsdevs {
+					if d.path == path {
+						return nil
+					}
+				}
+				if dev, err := setupJS(path); err == nil {
+					jsdevs = append(jsdevs, dev)
+				}
+			}
+			return nil
+		})
+		filepath.Walk("/dev", func(path string, info os.FileInfo, err error) error {
+			if strings.HasPrefix(filepath.Base(path), "hidraw") {
+				for _, d := range hiddevs {
+					if d.path == path {
+						return nil
+					}
+				}
+				if dev, err := setupHID(path); err == nil {
+					hiddevs = append(hiddevs, dev)
+				}
+			}
+			return nil
+		})
+	}
+	scan()
+
+	// Poll loop
+	for {
+		for _, d := range jsdevs {
+			d.readEvents()
+		}
+		for _, d := range hiddevs {
+			d.readEvents()
+		}
+		time.Sleep(10 * time.Millisecond)
+		scan()
+	}
+}
