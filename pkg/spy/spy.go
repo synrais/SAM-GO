b395bc47f0f1dd0e1c9f2facaf586ae5919a77b9
synrais <78070154+synrais@users.noreply.github.com>
2025-09-09 19:53:19 +1000

    Update spy.go

--- pkg/spy/spy.go
+++ pkg/spy/spy.go
@@ -2,26 +2,25 @@
 package spy
 
 import (
-	"encoding/binary"
+	"bytes"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
+	"select"
 	"syscall"
+	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
 
 const (
-	ReportSize = 64
-	Deadzone   = 15
+	REPORT_SIZE = 64
+	DEADZONE    = 15
 
-	// Joystick event types
+	// joystick constants
 	JS_EVENT_BUTTON = 0x01
 	JS_EVENT_AXIS   = 0x02
 	JS_EVENT_INIT   = 0x80
 
-	// ioctl constants
 	JSIOCGVERSION  = 0x80046a01
 	JSIOCGAXES     = 0x80016a11
 	JSIOCGBUTTONS  = 0x80016a12
@@ -30,159 +29,184 @@ const (
 	JSIOCGBTNMAP   = 0x80406a34
 )
 
-// RetroSpy style name maps
-var axisNames = map[uint32]string{
+// axis + button maps (retrospy style)
+var AXIS_NAMES = map[uint32]string{
 	0x00: "ABS_X", 0x01: "ABS_Y", 0x02: "ABS_Z",
 	0x03: "ABS_RX", 0x04: "ABS_RY", 0x05: "ABS_RZ",
 	0x10: "ABS_HAT0X", 0x11: "ABS_HAT0Y",
 	0x12: "ABS_HAT1X", 0x13: "ABS_HAT1Y",
 }
 
-var buttonNames = map[uint32]string{
+var BUTTON_NAMES = map[uint32]string{
 	0x120: "BTN_SOUTH", 0x121: "BTN_EAST", 0x122: "BTN_C", 0x123: "BTN_NORTH",
 	0x124: "BTN_WEST", 0x125: "BTN_Z", 0x126: "BTN_TL", 0x127: "BTN_TR",
 	0x128: "BTN_TL2", 0x129: "BTN_TR2", 0x12a: "BTN_SELECT", 0x12b: "BTN_START",
 	0x12c: "BTN_MODE", 0x12d: "BTN_THUMBL", 0x12e: "BTN_THUMBR",
 }
 
-// ---------------- Joystick ----------------
-type JSDevice struct {
-	fd      *os.File
-	name    string
-	axmap   []uint32
-	btnmap  []uint32
-	axes    map[string]int16
-	buttons map[string]uint8
+// small helper
+func ioctl(fd int, req uintptr, arg unsafe.Pointer) error {
+	_, _, errno := unix.Syscall(unix.SYS_IOCTL, uintptr(fd), req, uintptr(arg))
+	if errno != 0 {
+		return errno
+	}
+	return nil
 }
 
-func setupJSDevice(path string) (*JSDevice, error) {
-	fd, err := os.OpenFile(path, os.O_RDONLY|syscall.O_NONBLOCK, 0)
-	if err != nil {
-		return nil, err
-	}
+// ---------------- joystick setup ----------------
+type JSDevice struct {
+	fd       int
+	path     string
+	name     string
+	axes     map[string]int16
+	buttons  map[string]uint8
+	axmap    []uint32
+	btnmap   []uint32
+	version  uint32
+	numAxes  int
+	numBtns  int
+}
 
-	// version
+func SetupJSDevice(fd int, path string) (*JSDevice, error) {
 	var version uint32
-	if err := unix.IoctlGetUint32(int(fd.Fd()), JSIOCGVERSION, &version); err != nil {
+	if err := ioctl(fd, JSIOCGVERSION, unsafe.Pointer(&version)); err != nil {
 		return nil, err
 	}
 
-	// num axes
-	buf1 := make([]byte, 1)
-	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), JSIOCGAXES, uintptr(unsafe.Pointer(&buf1[0]))); errno != 0 {
-		return nil, fmt.Errorf("JSIOCGAXES: %v", errno)
-	}
-	numAxes := int(buf1[0])
-
-	// num buttons
-	buf2 := make([]byte, 1)
-	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), JSIOCGBUTTONS, uintptr(unsafe.Pointer(&buf2[0]))); errno != 0 {
-		return nil, fmt.Errorf("JSIOCGBUTTONS: %v", errno)
-	}
-	numButtons := int(buf2[0])
-
-	// name
-	nameBuf := make([]byte, 128)
-	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), JSIOCGNAME, uintptr(unsafe.Pointer(&nameBuf[0]))); errno != 0 {
-		return nil, fmt.Errorf("JSIOCGNAME: %v", errno)
-	}
-	name := strings.TrimRight(string(nameBuf), "\x00")
-
-	// axis map
-	axBuf := make([]byte, numAxes*4)
-	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), JSIOCGAXMAP, uintptr(unsafe.Pointer(&axBuf[0]))); errno != 0 {
-		return nil, fmt.Errorf("JSIOCGAXMAP: %v", errno)
-	}
-	axmap := make([]uint32, numAxes)
-	for i := 0; i < numAxes; i++ {
-		axmap[i] = binary.LittleEndian.Uint32(axBuf[i*4:])
+	var nAxes uint8
+	if err := ioctl(fd, JSIOCGAXES, unsafe.Pointer(&nAxes)); err != nil {
+		return nil, err
 	}
 
-	// button map
-	btnBuf := make([]byte, numButtons*4)
-	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), JSIOCGAXMAP, uintptr(unsafe.Pointer(&btnBuf[0]))); errno != 0 {
-		return nil, fmt.Errorf("JSIOCGAXMAP: %v", errno)
+	var nBtns uint8
+	if err := ioctl(fd, JSIOCGBUTTONS, unsafe.Pointer(&nBtns)); err != nil {
+		return nil, err
 	}
-	btnmap := make([]uint32, numButtons)
-	for i := 0; i < numButtons; i++ {
-		btnmap[i] = binary.LittleEndian.Uint32(btnBuf[i*4:])
+
+	buf := make([]byte, 128)
+	if err := ioctl(fd, JSIOCGNAME, unsafe.Pointer(&buf[0])); err != nil {
+		return nil, err
+	}
+	name := string(buf[:bytes.IndexByte(buf, 0)])
+
+	axmap := make([]uint32, nAxes)
+	if err := ioctl(fd, JSIOCGAXMAP, unsafe.Pointer(&axmap[0])); err != nil {
+		return nil, err
+	}
+
+	btnmap := make([]uint32, nBtns)
+	if err := ioctl(fd, JSIOCGBTNMAP, unsafe.Pointer(&btnmap[0])); err != nil {
+		return nil, err
 	}
 
 	fmt.Printf("Monitoring %s (joystick: %s)\n", path, name)
 	fmt.Printf("  Driver version: %d\n", version)
-	fmt.Printf("  Axes: %d  Buttons: %d\n", numAxes, numButtons)
+	fmt.Printf("  Axes: %d  Buttons: %d\n", nAxes, nBtns)
 
 	axes := make(map[string]int16)
 	for _, a := range axmap {
-		axes[axisNames[a]] = 0
+		axes[AXIS_NAMES[a]] = 0
 	}
 	buttons := make(map[string]uint8)
 	for _, b := range btnmap {
-		buttons[buttonNames[b]] = 0
+		buttons[BUTTON_NAMES[b]] = 0
 	}
 
-	return &JSDevice{fd, name, axmap, btnmap, axes, buttons}, nil
+	return &JSDevice{
+		fd:      fd,
+		path:    path,
+		name:    name,
+		axes:    axes,
+		buttons: buttons,
+		axmap:   axmap,
+		btnmap:  btnmap,
+		version: version,
+		numAxes: int(nAxes),
+		numBtns: int(nBtns),
+	}, nil
 }
 
-func (js *JSDevice) handleEvent(evt []byte) {
-	var time uint32 = binary.LittleEndian.Uint32(evt[0:4])
-	value := int16(binary.LittleEndian.Uint16(evt[4:6]))
-	etype := evt[6] & ^uint8(JS_EVENT_INIT)
-	num := evt[7]
+func (js *JSDevice) HandleEvent(evt []byte) {
+	var t uint32
+	var val int16
+	var etype, num uint8
+	_ = binary.Read(bytes.NewReader(evt), binary.LittleEndian, &t)
+	_ = binary.Read(bytes.NewReader(evt[4:6]), binary.LittleEndian, &val)
+	etype = evt[6]
+	num = evt[7]
+
+	etype &= ^uint8(JS_EVENT_INIT)
 
 	if etype == JS_EVENT_AXIS {
-		a := js.axmap[num]
-		js.axes[axisNames[a]] = value
+		code := js.axmap[num]
+		name := AXIS_NAMES[code]
+		if name == "" {
+			name = fmt.Sprintf("0x%x", code)
+		}
+		js.axes[name] = val
 	} else if etype == JS_EVENT_BUTTON {
-		b := js.btnmap[num]
-		js.buttons[buttonNames[b]] = uint8(value)
+		code := js.btnmap[num]
+		name := BUTTON_NAMES[code]
+		if name == "" {
+			name = fmt.Sprintf("0x%x", code)
+		}
+		if val != 0 {
+			js.buttons[name] = 1
+		} else {
+			js.buttons[name] = 0
+		}
 	}
 
-	axstr := []string{}
+	// dump RetroSpy-style
+	axs := ""
 	for k, v := range js.axes {
-		axstr = append(axstr, fmt.Sprintf("%s=%d", k, v))
+		axs += fmt.Sprintf("%s=%d ", k, v)
 	}
-	btnstr := []string{}
+	btns := ""
 	for k, v := range js.buttons {
-		btnstr = append(btnstr, fmt.Sprintf("%s=%d", k, v))
+		btns += fmt.Sprintf("%s=%d ", k, v)
 	}
-	fmt.Printf("[%s] Axes[%s] Buttons[%s] @%dms\n",
-		js.name, strings.Join(axstr, ", "), strings.Join(btnstr, ", "), time)
+	fmt.Printf("[%s] Axes[%s] Buttons[%s]\n", js.name, axs, btns)
 }
 
-// ---------------- HID ----------------
-func decodeHID(data []byte) string {
-	// TODO: implement your keyboard/mouse/gamepad decode from Python
-	return fmt.Sprintf("HID raw: %x", data)
-}
+// ---------------- monitor ----------------
+func MonitorAll() {
+	var devices []*JSDevice
 
-// ---------------- Monitor ----------------
-func Run() {
-	// hidraw devices
-	hids, _ := filepath.Glob("/dev/hidraw*")
-	for _, path := range hids {
-		fmt.Printf("Monitoring %s (hidraw)\n", path)
-	}
-
-	// joystick devices
-	jsdevs := []*JSDevice{}
-	jslist, _ := filepath.Glob("/dev/input/js*")
-	for _, path := range jslist {
-		js, err := setupJSDevice(path)
+	// open /dev/input/js*
+	entries, _ := filepath.Glob("/dev/input/js*")
+	for _, path := range entries {
+		fd, err := unix.Open(path, unix.O_RDONLY|unix.O_NONBLOCK, 0)
 		if err != nil {
-			fmt.Println("Error setting up", path, ":", err)
+			fmt.Println("open failed:", err)
 			continue
 		}
-		jsdevs = append(jsdevs, js)
+		js, err := SetupJSDevice(fd, path)
+		if err != nil {
+			fmt.Println("setup failed:", err)
+			continue
+		}
+		devices = append(devices, js)
 	}
 
-	// event loop
+	fds := make([]unix.PollFd, len(devices))
+	for i, d := range devices {
+		fds[i] = unix.PollFd{Fd: int32(d.fd), Events: unix.POLLIN}
+	}
+
 	for {
-		for _, js := range jsdevs {
-			buf := make([]byte, 8)
-			n, _ := js.fd.Read(buf)
-			if n == 8 {
-				js.handleEvent(buf)
+		_, err := unix.Poll(fds, -1)
+		if err != nil {
+			fmt.Println("poll failed:", err)
+			break
+		}
+		for i, fd := range fds {
+			if fd.Revents&unix.POLLIN != 0 {
+				buf := make([]byte, 8)
+				n, _ := unix.Read(devices[i].fd, buf)
+				if n == 8 {
+					devices[i].HandleEvent(buf)
+				}
 			}
 		}
 	}
