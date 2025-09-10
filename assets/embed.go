297d08ff553280cc9decbacc5f1dff6b0418f0c4
synrais <78070154+synrais@users.noreply.github.com>
2025-09-11 04:01:17 +1000

    Update embed.go

--- assets/embed.go
+++ assets/embed.go
@@ -1,30 +1,8 @@
-package embed
+package assets
 
 import (
-	_ "embed" // Required to embed files into Go source
-	"strings"
+    _ "embed" // Required to embed files
 )
 
 //go:embed gamecontrollerdb.txt
-var gamecontrollerdb string
-
-// Add more embedded files as needed
-//go:embed otherfile.txt
-var otherFile string
-
-// Function to load the gamecontrollerdb as a slice of strings
-func LoadGameControllerDB() ([]string, error) {
-	lines := strings.Split(gamecontrollerdb, "\n")
-	var controllers []string
-	for _, line := range lines {
-		if len(line) > 0 {
-			controllers = append(controllers, line)
-		}
-	}
-	return controllers, nil
-}
-
-// Another function to load other files, if necessary
-func LoadOtherFile() string {
-	return otherFile
-}
+var GameControllerDB string
