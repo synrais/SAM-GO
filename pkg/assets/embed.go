6f048d84e40d70a0239f02219323fb89946bd088
synrais <78070154+synrais@users.noreply.github.com>
2025-09-22 08:37:52 +1000

    Update embed.go

--- pkg/assets/embed.go
+++ pkg/assets/embed.go
@@ -1,8 +1,15 @@
 package assets
 
 import (
-	_ "embed" // Required to embed files
+	"archive/zip"
+	"bytes"
+	_ "embed"
+	"io"
+	"os"
+	"path/filepath"
 )
+
+// --- Embeds ---
 
 //go:embed gamecontrollerdb.txt
 var GameControllerDB string
@@ -15,3 +22,50 @@ var DefaultSAMIni []byte
 
 //go:embed mplayer.zip
 var MPlayerZip []byte
+
+// --- Helpers ---
+
+// ExtractZipBytes extracts an embedded zip (like MPlayerZip) into destDir.
+func ExtractZipBytes(data []byte, destDir string) error {
+	r, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		return err
+	}
+
+	for _, f := range r.File {
+		fpath := filepath.Join(destDir, f.Name)
+
+		if f.FileInfo().IsDir() {
+			if err := os.MkdirAll(fpath, f.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// Ensure parent directory exists
+		if err := os.MkdirAll(filepath.Dir(fpath), 0755); err != nil {
+			return err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		dst, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, f.Mode())
+		if err != nil {
+			rc.Close()
+			return err
+		}
+
+		if _, err := io.Copy(dst, rc); err != nil {
+			dst.Close()
+			rc.Close()
+			return err
+		}
+
+		dst.Close()
+		rc.Close()
+	}
+	return nil
+}
