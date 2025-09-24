6561506a327d4fb0d6213ca52af4ed79e08065e1
synrais <78070154+synrais@users.noreply.github.com>
2025-09-24 12:53:48 +1000

    Create gamemenu.go

---
+++ pkg/attract/gamemenu.go
@@ -0,0 +1,14 @@
+package attract
+
+import "fmt"
+
+// Each of these is its own "test menu"
+func GameMenu1() { fmt.Println("[GameMenu1] Running test menu 1") }
+func GameMenu2() { fmt.Println("[GameMenu2] Running test menu 2") }
+func GameMenu3() { fmt.Println("[GameMenu3] Running test menu 3") }
+func GameMenu4() { fmt.Println("[GameMenu4] Running test menu 4") }
+func GameMenu5() { fmt.Println("[GameMenu5] Running test menu 5") }
+func GameMenu6() { fmt.Println("[GameMenu6] Running test menu 6") }
+func GameMenu7() { fmt.Println("[GameMenu7] Running test menu 7") }
+func GameMenu8() { fmt.Println("[GameMenu8] Running test menu 8") }
+func GameMenu9() { fmt.Println("[GameMenu9] Running test menu 9") }
