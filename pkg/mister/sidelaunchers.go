dd12c67d40dee1764dbe1d3ed95f4b69cc4c8c02
synrais <78070154+synrais@users.noreply.github.com>
2025-09-28 16:35:41 +1000

    Update sidelaunchers.go

--- pkg/mister/sidelaunchers.go
+++ pkg/mister/sidelaunchers.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/bendahl/uinput"
 	"github.com/synrais/SAM-GO/pkg/assets"
 	"github.com/synrais/SAM-GO/pkg/config"
 	"github.com/synrais/SAM-GO/pkg/input/virtualinput"
